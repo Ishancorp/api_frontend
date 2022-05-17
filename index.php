9fc9ef94f860a66b1e3a66fbeed919c994b86b27
Ishan Sharma <sharma.ishan450@gmail.com>
2022-05-17 08:53:44 -0400

    a

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
-$my_env_var = getenv('API_KEY');
-
 <script type="text/javascript">
-   var php_var = "<?php echo $my_env_var; ?>";
+   $my_env_var = getenv('API_KEY');
+   const API_KEY = "<?php echo $my_env_var; ?>";
 </script>
 
 <?php include_once("home.html"); ?>
